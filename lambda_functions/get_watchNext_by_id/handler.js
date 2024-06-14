67232aa057d08cc0cb664e76d7c2a721abf7bdf1
sasha-cattaneo <s.cattaneo29@studenti.unibg.it>
2024-06-14 12:18:03 +0200

    Update handler.js

--- lambda_functions/get_watchNext_by_id/handler.js
+++ lambda_functions/get_watchNext_by_id/handler.js
@@ -20,13 +20,6 @@ module.exports.get_by_id = (event, context, callback) => {
         })
     }
     
-    if (!body.doc_per_page) {
-        body.doc_per_page = 10
-    }
-    if (!body.page) {
-        body.page = 1
-    }
-    
     connect_to_db().then(() => {
         console.log('=> get_all talks');
         talk.find({_id: body.id}, { related_videos: 1})
